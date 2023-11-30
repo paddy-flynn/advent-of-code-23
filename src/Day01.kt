acec39a934b334d5f822678ad467f4d77005511f
Paddy Flynn <pflynn210392@gmail.com>
2023-11-30 23:28:39 +0000

    inital commit

---
+++ src/Day01.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 0)
+
+    val input2 = readInput("Day01")
+    part1(input2).println()
+    part2(input2).println()
+}
